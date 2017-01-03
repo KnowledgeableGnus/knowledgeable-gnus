2713aef78eff0a8d72c286d21684a801fc1fda08
Steve Graves <steve.graves86@gmail.com>
2017-01-03 15:07:07 -0800

    Establish initial server and database

--- server/controllers.js
+++ server/controllers.js
@@ -1,15 +1,66 @@
-var models = require('models.js');
+var models = require('./models.js');
 
 module.exports = {
 
   users: {
-
+    get: function(req, res) {
+      models.users.get(function(err, results) {
+        if (err) {
+          //console.log for now, handle appropriatly eventually
+          console.log('error: ', err);
+        }
+        res.json(results);
+      });
+    },
+    post: function(req, res) {
+      var params = [req.body.username, req.body.password, req.body.address, req.body.email, req.body.createdAt];
+      models.users.post(params, function(err, results) {
+        if (err) {
+          console.log('error: ', err);
+        }
+        res.send(201);
+      });
+    }
   },
   coordinates: {
-
+    get: function(req, res) {
+      var params = [req.body.id_users];
+      models.coordinates.get(params, function(err, results) {
+        if (err) {
+          console.log('error: ', err);
+        }
+        res.json(results);
+      });
+    },
+    post: function(req, res) {
+      var params = [req.body.id_users, req.body.time, req.body.lat, req.body.lng];
+      models.coordinates.post(params, function(err, results) {
+        if (err) {
+          console.log('error: ', err);
+        }
+        res.send(201);
+      });
+    }
   },
   locations: {
-
+    get: function (req, res) {
+      var params = [req.body.id_users];
+      models.locations.get(params, function(err, results) {
+        if (err) {
+          console.log('error: ', err);
+        }
+        res.json(results);
+      });
+    },
+    post: function (req, res) {
+      var params = [req.body.id_users, req.body.name, req.body.lat, req.body.lng];
+      models.locations.post(params, function(err, results) {
+        if (err) {
+          console.log('error: ', err);
+        }
+        res.send(201);
+      })
+    }
   }
 
 };
